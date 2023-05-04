c9cc91d6ea68d8db3594e5eca95da10a08964211
Harsh@0196 <harsh@hashtrust.in>
2023-05-04 21:50:38 +0530

    initial notifications setup

---
+++ notifications/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+class CreatedUpdatedMixin(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        help_text=_("The creation time, always in Asia/Tokyo timezone."),
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        help_text=_("The last update time, always in Asia/Tokyo timezone."),
+    )
+
+class Category(CreatedUpdatedMixin):
+    title = models.CharField(max_length=30)
+    icon = models.ImageField(upload_to="category/icons",null=True,blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+class Notification(CreatedUpdatedMixin):
+
+    class NotificationTypes(models.TextChoices):    
+        success = "success", "Success"
+        error = "error", "Error"
+        warning = "warning", "Warning"
+        info = "info", "Information"
+
+    class NotificationStatus(models.TextChoices):
+        read = "read", "Read"
+        unread = "unread", "Unread"
+        deleted = "deleted", "Deleted"
+
+    title = models.CharField(max_length=265)
+    message = models.TextField()
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True,blank=True)
+    receivers = models.ManyToManyField(User, related_name="receivers_notifications")
+    sender = models.ForeignKey(User,on_delete=models.CASCADE)
+    notification_type = models.CharField(choices=NotificationTypes.choices,max_length=20)
+    notification_status = models.CharField(choices=NotificationStatus.choices,max_length=20)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+
---
+++ notifications/utils.py
@@ -0,0 +1,81 @@
+from notifications.models import Notification, Category
+from django.contrib.auth import get_user_model
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+
+User = get_user_model()
+def send_notification(
+        title : str,
+        receivers : list[User],
+        sender : User,
+        notification_type : str,
+        message : str = None,
+        category : Category = None,
+        notification_status : str = Notification.NotificationStatus.unread,
+        real_time_notification : bool = False
+    ):
+    """
+    This function will create in app notification with required details. 
+
+    Parameters:
+    1. title : string
+    2. receivers : List of User model instance
+    3. sender : User model instance
+    4. notification_type : 
+        a. success
+        b. error
+        c. warning
+        d. info
+    5. message : string
+    6. category : Category Model instance
+    7. notification_status : 
+        a. read
+        b. unread
+        c. deleted
+    8. real_time_notification : bool (Set True if you want to implement Real Time Notifications)
+    """
+    if len(receivers) == 0:
+        return "Please provide atleast one receiver."
+    if notification_type not in list(dict(Notification.NotificationTypes.choices).keys()):
+        return "Please provide valida notification type. It should be any of ['success', 'error', 'warning', 'info']."
+    if notification_status not in list(dict(Notification.NotificationStatus.choices).keys()):
+        return "Please provide valida notification status. It should be any of ['read', 'unread', 'deleted']."
+
+    notification = Notification.objects.create(
+        title = title,
+        message = message,
+        category = category,
+        sender = sender,
+        notification_type = notification_type,
+        notification_status = notification_status
+    )
+    for receiver in receivers:
+        notification.receivers.add(receiver)
+    
+    if real_time_notification:
+        channel_layer = get_channel_layer()
+        # Trigger message sent to group
+        async_to_sync(channel_layer.group_send)(
+            str(sender.pk),  # Group Name, Should always be string
+            {
+                "type": "notify",   # Custom Function written in the consumers.py
+                "message": {"title":title,"message":message},
+            },
+        )
+
+def get_notifications(user, notification_status = None):
+    if not notification_status:
+        return Notification.objects.prefetch_related('receivers').filter(receivers = user)
+    
+    if notification_status not in list(dict(Notification.NotificationStatus.choices).keys()):
+        return "Please provide valid notification status. It should be any of ['read', 'unread', 'deleted']." 
+    else:
+        return Notification.objects.prefetch_related('receivers').filter(receivers = user,notification_status = notification_status)
+    
+
+def mark_as_read(user):
+    Notification.objects.filter(receivers = user,notification_status = Notification.NotificationStatus.unread ).update(notification_status = Notification.NotificationStatus.read)
+
+def mark_as_unread(user):
+    Notification.objects.filter(receivers = user,notification_status = Notification.NotificationStatus.read ).update(notification_status = Notification.NotificationStatus.unread)
