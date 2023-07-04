5bd7a6bc6f668cfd9f98930bdf128b7b8de49347
Harsh-0196 <harsh@hashtrust.in>
2023-07-04 18:12:53 +0530

    Update setup files

--- setup.py
+++ setup.py
@@ -1,3 +1,27 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="django-app-notifications",
+    version="{{PACKAGE_VERSION}}",
+    description="A Django Library to send notifications",
+    readme="README.md",
+    long_description="file: README.md",
+    long_description_content_type="text/markdown",
+    url="https://www.example.com/",
+    author="Hashtrust Technologies Private Limited",
+    author_email="support@hashtrust.in",
+    license="MIT License",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django :: 4.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Communications :: Chat",
+    ],
+)
